d024cbad17983832f5454d1c673cf0ca1ea31ed4
Nokskiy <tsfkafsu@yandex.ru>
2025-08-13 18:36:16 +0500

    Option

---
+++ src/Option.cs
@@ -0,0 +1,9 @@
+﻿namespace SCLP;
+
+public class Option<T>(string shortName, string fullName, string? description, T? argument)
+{
+    public string ShortName => shortName;
+    public string FullName => fullName;
+    public string? Description => description;
+    public T? Argument => argument;
+}
