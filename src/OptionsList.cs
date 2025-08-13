f18d7b1cfab4558f257e0f58eaa778750478b391
Nokskiy <tsfkafsu@yandex.ru>
2025-08-13 18:59:46 +0500

    Options list

---
+++ src/OptionsList.cs
@@ -0,0 +1,8 @@
+﻿namespace SCLP;
+
+public class OptionsList(Option[] options)
+{
+    public List<Option> Options { get; private set; } = options != null ? options.ToList() : new List<Option>();
+
+    public void AddOption(ref Option option) => Options.Add(option);
+}
